c5f980f4ed6f281f555c13c0c17847eef60019bd
Roger Eduardo de Couto <rogerecouto@gmail.com>
2020-02-22 12:39:13 -0300

    Hello Wolrd

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Projeto desenvolvido no curso rest com spring boot da udemy
---
+++ src/main/java/br/com/erudio/controllers/GreetingController.java
@@ -0,0 +1,23 @@
+package br.com.erudio.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.Greeting;
+
+@RestController
+public class GreetingController {
+
+	public static final String template = "Hello, %s";
+	
+	public final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greeting")
+	public Greeting greetingReq(@RequestParam(value="name", defaultValue = "World")String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+}
